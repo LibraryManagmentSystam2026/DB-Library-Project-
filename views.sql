e70765d28bb067583d4e27eb9c3b0ecccd3357b1
Noshin Rabani <f25605363@nutech.edu.pk>
2026-06-07 08:58:55 +0500

    Update views.sql

--- views.sql
+++ views.sql
@@ -1,11 +1,11 @@
-CREATE VIEW LoanDetails AS
-SELECT 
-    m.Name AS MemberName,
-    b.Title AS BookTitle,
-    l.Name AS LibrarianName,
-    ln.IssueDate,
-    ln.DueDate
-FROM Loan ln
-JOIN Member m ON ln.MemberID = m.MemberID
-JOIN Book b ON ln.BookID = b.BookID
-JOIN Librarian l ON ln.LibrarianID = l.LibrarianID;
+CREATE VIEW ActiveLoans AS
+SELECT m.Name AS MemberName, b.Title, l.IssueDate, l.DueDate
+FROM Loan l
+JOIN Member m ON l.MemberID = m.MemberID
+JOIN Book b ON l.BookID = b.BookID;
+
+CREATE VIEW BookAuthors AS
+SELECT b.Title, a.AuthorName
+FROM BookAuthor ba
+JOIN Book b ON ba.BookID = b.BookID
+JOIN Author a ON ba.AuthorID = a.AuthorID;
