648b80e89f11bb44aee54b211961abddd9cfeb55
Noshin Rabani <f25605363@nutech.edu.pk>
2026-06-10 02:18:14 +0500

    Update views.sql

--- views.sql
+++ views.sql
@@ -1,11 +1,26 @@
 CREATE VIEW ActiveLoans AS
-SELECT m.Name AS MemberName, b.Title, l.IssueDate, l.DueDate
+SELECT 
+    m.Name AS MemberName, 
+    b.Title, 
+    l.IssueDate, 
+    l.DueDate
 FROM Loan l
 JOIN Member m ON l.MemberID = m.MemberID
 JOIN Book b ON l.BookID = b.BookID;
+GO
+
+SELECT * FROM ActiveLoans;
+
+--VIEW 2
+IF OBJECT_ID('BookAuthors', 'V') IS NOT NULL
+    DROP VIEW BookAuthors;
+GO
 
 CREATE VIEW BookAuthors AS
 SELECT b.Title, a.AuthorName
 FROM BookAuthor ba
-JOIN Book b ON ba.BookID = b.BookID
-JOIN Author a ON ba.AuthorID = a.AuthorID;
+INNER JOIN Book b ON ba.BookID = b.BookID
+INNER JOIN Author a ON ba.AuthorID = a.AuthorID;
+GO
+
+SELECT * FROM BookAuthors;
