832cd0773fecad40a5ac860629745552af567a5d
Noshin Rabani <f25605363@nutech.edu.pk>
2026-06-10 01:49:07 +0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,30 +1,25 @@
--- =========================
--- ADVANCED QUERIES
--- =========================
-
-USE LibraryDB;
-
--- JOIN (3 tables)
+--1. JOIN (3 tables)
 SELECT m.Name, b.Title, l.IssueDate
 FROM Loan l
 JOIN Member m ON l.MemberID = m.MemberID
 JOIN Book b ON l.BookID = b.BookID;
 
--- AGGREGATION
+--2. AGGREGATION (COUNT + GROUP BY)
 SELECT MemberID, COUNT(*) AS TotalBooks
 FROM Loan
 GROUP BY MemberID
 HAVING COUNT(*) > 1;
 
--- SUBQUERY (IN)
+--3. SUBQUERY (IN)
 SELECT Title
 FROM Book
 WHERE BookID IN (SELECT BookID FROM Loan);
 
--- SUBQUERY (EXISTS)
+--4. SUBQUERY (EXISTS)
 SELECT Name
 FROM Member m
 WHERE EXISTS (
     SELECT 1 FROM Loan l
     WHERE l.MemberID = m.MemberID
 );
+
