dc41e8f74eae1f03457dee49c5148bc78db58d48
Noshin Rabani <f25605363@nutech.edu.pk>
2026-06-11 07:51:56 +0500

    Update triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -2,16 +2,18 @@
 -- TRIGGERS
 -- =========================
 
-USE LibraryDB;
-
-DELIMITER //
-
 CREATE TRIGGER LoanInsert
-AFTER INSERT ON Loan
-FOR EACH ROW
+ON Loan
+AFTER INSERT
+AS
 BEGIN
-    INSERT INTO LoanAudit(MemberID, BookID, ActionDate, ActionType)
-    VALUES (NEW.MemberID, NEW.BookID, NOW(), 'ISSUE');
-END//
-
-DELIMITER ;
+    SET NOCOUNT ON;
+    INSERT INTO LoanAudit (MemberID, BookID, ActionDate, ActionType)
+    SELECT inserted.MemberID, inserted.BookID, GETDATE(), 'ISSUE'
+    FROM inserted;
+END;
+GO
+INSERT INTO Loan
+VALUES
+(16,1,2,1,'2026-02-01','2026-02-10','2026-02-09');
+SELECT * FROM LoanAudit;
